ea6b77e4f6c528053217084181e4d927f21ff59c
Rodrigo Felix <rodrigofx.dev@gmail.com>
2023-10-23 16:35:26 -0300

    added readme, image and video | done the calculator is work

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function calculate(nType,nValue){
-    console.log(nType, nValue)
+    if (nType === 'action'){
+        if(nValue === 'c'){
+            document.getElementById('results').value = ''
+        }
+        if(nValue === '/'||nValue === '*'||nValue === '-'|| nValue === '+'|| nValue === '.'){
+            document.getElementById('results').value += nValue
+        }
+        if(nValue === '='){
+            var valueField = eval(document.getElementById('results').value)
+            document.getElementById('results').value = valueField
+        }
+    }else if(nType === 'value'){
+        document.getElementById('results').value += nValue
+    }
 }
